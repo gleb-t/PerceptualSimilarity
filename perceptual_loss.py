a640b07ef60753d3677df83fa66b62d8b952fbff
gleb-t <gleb-t@users.noreply.github.com>
2020-07-08 13:18:19 +0200

    First working version of maintaining distances.

--- perceptual_loss.py
+++ perceptual_loss.py
@@ -3,51 +3,74 @@ from __future__ import absolute_import
 
 import sys
 import scipy
+import imageio
 import scipy.misc
 import numpy as np
 import torch
 from torch.autograd import Variable
 import models
 
+from typing import *
+
 use_gpu = True
 
 ref_path  = './imgs/ex_ref.png'
 pred_path = './imgs/ex_p1.png'
 
-ref_img = scipy.misc.imread(ref_path).transpose(2, 0, 1) / 255.
-pred_img = scipy.misc.imread(pred_path).transpose(2, 0, 1) / 255.
+ref_img = imageio.imread(ref_path).transpose(2, 0, 1) / 255.
+pred_img = imageio.imread(pred_path).transpose(2, 0, 1) / 255.
+
+gpu = torch.device('cuda')
 
 # Torchify
-ref = Variable(torch.FloatTensor(ref_img)[None,:,:,:])
-pred = Variable(torch.FloatTensor(pred_img)[None,:,:,:], requires_grad=True)
+# ref = torch.tensor(ref_img[np.newaxis, ...]).to(gpu)
+# pred = torch.tensor(pred_img[np.newaxis, ...], requires_grad=True).to(gpu)
 
-loss_fn = models.PerceptualLoss(model='net-lin', net='vgg', use_gpu=use_gpu)
-optimizer = torch.optim.Adam([pred,], lr=1e-3, betas=(0.9, 0.999))
+# Generate a random distance matrix.
+dataSize = 8
+# Make a matrix with positive values.
+distancesCpu = np.clip(np.random.normal(0.5, 1.0 / 3, (dataSize, dataSize)), 0, 1)
+# Make it symmetrical.
+distancesCpu = np.matmul(distancesCpu, distancesCpu.T)
+
+imagesCpu = np.clip(np.random.normal(0.5, 0.5 / 3, (dataSize, 3, 64, 64)), 0, 1)
+images = torch.tensor(imagesCpu, requires_grad=True, dtype=torch.float32, device=gpu)
+
+scale = torch.tensor(1.0, requires_grad=True, dtype=torch.float32, device=gpu)
+
+
+lossModel = models.PerceptualLoss(model='net-lin', net='vgg', use_gpu=use_gpu).to(gpu)
+optimizer = torch.optim.Adam([images, scale], lr=1e-3, betas=(0.9, 0.999))
+bs = 8
 
 import matplotlib.pyplot as plt
 plt.ion()
-fig = plt.figure(1)
-ax = fig.add_subplot(131)
-ax.imshow(ref_img.transpose(1, 2, 0))
-ax.set_title('target')
-ax = fig.add_subplot(133)
-ax.imshow(pred_img.transpose(1, 2, 0))
-ax.set_title('initialization')
+fig, axes = plt.subplots(nrows=2, ncols=bs // 2)
 
-for i in range(1000):
-    dist = loss_fn.forward(pred, ref, normalize=True)
+for i in range(10000):
+
+    # randomIndices = np.random.randint(0, dataSize, bs).tolist()  # type: List[int]
+    randomIndices = list(range(dataSize))  # type: List[int]
+    distanceBatch = torch.tensor(distancesCpu[randomIndices, :][:, randomIndices], dtype=torch.float32, device=gpu)
+    imageBatch = images[randomIndices].contiguous()
+
+    distPred = lossModel.forward(imageBatch.repeat(repeats=(bs, 1, 1, 1)).contiguous(),
+                                 imageBatch.repeat_interleave(repeats=bs, dim=0).contiguous(), normalize=True)
+    distPredMat = distPred.reshape((bs, bs))
+
+    loss = torch.sum((distanceBatch - distPredMat * scale) ** 2)  # MSE
+
     optimizer.zero_grad()
-    dist.backward()
+    loss.backward()
     optimizer.step()
-    pred.data = torch.clamp(pred.data, 0, 1)
+    images.data = torch.clamp(images.data, 0, 1)
     
     if i % 10 == 0:
-        print('iter %d, dist %.3g' % (i, dist.view(-1).data.cpu().numpy()[0]))
-        pred_img = pred[0].data.cpu().numpy().transpose(1, 2, 0)
-        pred_img = np.clip(pred_img, 0, 1)
-        ax = fig.add_subplot(132)            
-        ax.imshow(pred_img)
-        ax.set_title('iter %d, dist %.3f' % (i, dist.view(-1).data.cpu().numpy()[0]))
+        msg = 'iter {}, loss {:.3f}, scale: {:.3f}'.format(i, loss.item(), scale.item())
+        print(msg)
+        for i, ax in enumerate(axes.flatten()):
+            ax.imshow(imageBatch.cpu().data.numpy()[i].transpose(1, 2, 0))
+        fig.suptitle(msg)
         plt.pause(5e-2)
         # plt.imsave('imgs_saved/%04d.jpg'%i,pred_img)
 
